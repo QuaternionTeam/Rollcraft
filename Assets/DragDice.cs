658f35b8a65a594c3ab38960fc400680c720a01d
x <alvear.devs@gmail.com>
2024-11-23 18:02:54 -0300

    Interfaz de acción - Ataque

--- Assets/DragDice.cs
+++ Assets/DragDice.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class DragDice : MonoBehaviour
 {
@@ -6,11 +8,17 @@ public class DragDice : MonoBehaviour
 
     public Vector3 initialPosition;
 
+    public RawImage diceImage;
+
+    public bool diceClicked;
+    public bool target;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //initialPosition = 
+        initialPosition = diceImage.rectTransform.position;
+
     }
 
     // Update is called once per frame
@@ -18,15 +26,42 @@ public class DragDice : MonoBehaviour
     {
         if(dragging)
         {
-            Vector3 mousePOsition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            Vector3 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+
+            diceImage.rectTransform.position = mousePosition;
         }
+        
     }
 
+
+    public void OnMouseOver()
+    {
+
+    }
 
     public void OnMouseDown()
     {
         dragging = true;
         print(dragging);
+    }
+
+    public void OnMouseUp()
+    {
+        dragging=false;
+        if(target)
+        {
+            // utiliza poder del coso
+
+        }
+
+         diceImage.rectTransform.position = initialPosition;
+
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if(collider.gameObject.GetComponent<Rigidbody2D>() != null)
+        target = true;
     }
 
 }
